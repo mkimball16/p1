a37fc929a1593afaf5e83bbd268ab8c635dd6299
MacKenzie Kimball <mackenziekimball@gmail.com>
2014-09-15 22:30:05 -0400

    updated typo

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ View: [link] Github: [link] </p>
 <p>Last May, my web developer left the organization. So I taught myself HTML and a little CSS and took over maintenance of the site. In doing so, I quickly discovered that I really enjoy coding and would like to expand my knowledge in the area.</p>
 <p>This is my second course in Information Technology at Harvard Extension School. Last semester I learned the basics of HTML, CSS, and a little PHP, Java, and jQuery. I am enrolled in this class to expand my coding knowledge and take the next step in learning how to develop web applications and dynamic websites. I am working on getting a Certificate in Web Technologies and potentially proceeding on to a Masters in IT. I am really looking forward to learning as much as possible from this course!</p>
 <h2>What previous experience do you have with programming?</h2>
-<p>I have an intermediate knowledge of HTMl and CSS and a beginners knowledge of PHP, Java, and jQuery. But I am a fast an eager learner, so I am confident this course really expand my knowledge and skills.</p>
+<p>I have an intermediate knowledge of HTML and CSS and a beginners knowledge of PHP, Java, and jQuery. But I am a fast an eager learner, so I am confident this course really expand my knowledge and skills.</p>
 <h2>Are you working on a Mac, PC, or some other platform?</h2>
 <p>I am working on a Mac.</p>
 <h2>Anything else we should know?</h2>
