b6faf87d9fec3e4acf7f606b8cc0f8ca955abf50
MacKenzie Kimball <mackenziekimball@gmail.com>
2014-09-18 08:23:59 -0400

    added bootstrap and modified css

--- index.php
+++ index.php
@@ -5,28 +5,49 @@
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 <title>MacKenzie Kimball</title>
 <link rel="stylesheet" href="css/style.css" type="text/css" />
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="container theme-showcase" role="main">
+<div class="jumbotron">
 <h1>MacKenzie Kimball<br />
 Dynamic Web Applications</h1>
 <img src="images/me.JPG" alt="MacKenzie" width="400">
+</div>
+ <div class="page-header">
 <h2>Projects</h2>
+</div>
 <p><strong>Project 2</strong><br />
-View: <a href="http://p2.mackenziek.me/" target="_blank">http://p2.mackenziek.me/ </a> Github: <a href="https://github.com/mkimball16/p2" target="_blank">https://github.com/mkimball16/p2</a> </p>
+<a type="button" class="btn btn-lg btn-info" href="http://p2.mackenziek.me/" target="_blank">Live</a>
+<a type="button" class="btn btn-lg btn-info" href="https://github.com/mkimball16/p2" target=_blank">Github</a></p>
 <p><strong>Project 3</strong><br />
-View: <a href="http://p3.mackenziek.me/" target="_blank">http://p3.mackenziek.me/ </a> Github: <a href="https://github.com/mkimball16/p3" target="_blank">https://github.com/mkimball16/p3</a> </p>
+<a type="button" class="btn btn-lg btn-info" href="http://p3.mackenziek.me/" target="_blank">Live</a>
+<a type="button" class="btn btn-lg btn-info" href="https://github.com/mkimball16/p3" target=_blank">Github</a></p>
 <p><strong>Project 4</strong><br />
-View: <a href="http://p4.mackenziek.me/" target="_blank">http://p4.mackenziek.me/ </a> Github: <a href="https://github.com/mkimball16/p4" target="_blank">https://github.com/mkimball16/p4</a> </p>
+<a type="button" class="btn btn-lg btn-info" href="http://p4.mackenziek.me/" target="_blank">Live</a>
+<a type="button" class="btn btn-lg btn-info" href="https://github.com/mkimball16/p4" target=_blank">Github</a></p>
+ <div class="page-header">
 <h2>In 2+ sentences describe who you are and what you do.</h2>
+</div>
 <p>Hi! My name is MacKenzie Kimball. I currently work as a Web Editor at <a href="http://www.dana-farber.org/" target="blank">Dana-Farber Cancer Institute</a> here in Boston. I have been in publishing for eight years, mainly on the editorial side of the business. I moved over to the digital side a few years ago with creating and managing blogs, enewsletters, and elearning courses. I am originally from Massachusetts and currently live in South Boston. I went to school at the University of Vermont and graduated with a double degree in English and Psychology.</p>
+ <div class="page-header">
 <h2>Why are you taking this course?</h2>
+</div>
 <p>Last May, my web developer left the organization. So I taught myself HTML and a little CSS and took over maintenance of the site. In doing so, I quickly discovered that I really enjoy coding and would like to expand my knowledge in the area.</p>
 <p>This is my second course in Information Technology at Harvard Extension School. Last semester I learned the basics of HTML, CSS, and a little PHP, Java, and jQuery. I am enrolled in this class to expand my coding knowledge and take the next step in learning how to develop web applications and dynamic websites. I am working on getting a Certificate in Web Technologies and potentially proceeding on to a Masters in IT. I am really looking forward to learning as much as possible from this course!</p>
+ <div class="page-header">
 <h2>What previous experience do you have with programming?</h2>
+</div>
 <p>I have an intermediate knowledge of HTML and CSS and a beginners knowledge of PHP, Java, and jQuery. But I am a fast an eager learner, so I am confident this course really expand my knowledge and skills.</p>
+ <div class="page-header">
 <h2>Are you working on a Mac, PC, or some other platform?</h2>
+</div>
 <p>I am working on a Mac.</p>
+ <div class="page-header">
 <h2>Anything else we should know?</h2>
 <p>In addition to my day job at Dana-Farber, I am also interning with an event planning company. Last semester I created a very rudimentary website for my potential party planning company. I am interested in exploring ways to enhance that website and make it more dynamic. I also have an idea for a dynamic web application for a non-profit my father volunteers with, Little Brothers Friends of the Elderly. I hope to explore these ideas a little further in this class.</p>
+</div>
+</div>
 </body>
 </html>
